71457dac5798168c4f7f855911e0a5767b3b371a
alsysew <andredesanti@gmail.com>
2021-11-11 18:09:17 -0300

    Modulos - correção

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+
 resource "aws_instance" "web" {
   ami           = "ami-0856ceeafc1be110c"
   instance_type = "t2.micro"
+  key_name = "Treinamento-dia2-keypair"
   associate_public_ip_address = true
   root_block_device {
     encrypted = true
@@ -9,5 +15,56 @@ resource "aws_instance" "web" {
   tags = {
     Name = "${var.nome}",
     Itau = true
+  }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id= "vpc-05f2dce4567354bbd"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups = null,
+      self            = null
+    },
+    {
+      description      = "SSH from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups = null,
+      self            = null
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"],
+      prefix_list_ids  = null,
+      security_groups  = null,
+      self             = null,
+      description      = "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
