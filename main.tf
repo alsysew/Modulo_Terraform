c695693530ff279d0fc886363f61535180838002
alsysew <andredesanti@gmail.com>
2021-11-12 09:08:56 -0300

    Modulos - última atualização

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   key_name = "Treinamento-dia2-keypair"
   associate_public_ip_address = true
+  subnet_id = "subnet-0f4dc32624c2fb345"
   root_block_device {
     encrypted = true
     volume_size = 8
@@ -21,50 +22,50 @@ resource "aws_instance" "web" {
 
 
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id= "vpc-05f2dce4567354bbd"
-
-  ingress = [
-    {
-      description      = "SSH from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups = null,
-      self            = null
-    },
-    {
-      description      = "SSH from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups = null,
-      self            = null
-    }
-  ]
-
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"],
-      prefix_list_ids  = null,
-      security_groups  = null,
-      self             = null,
-      description      = "Libera dados da rede interna"
-    }
-  ]
-
-  tags = {
-    Name = "allow_ssh"
-  }
+#  name        = "allow_ssh"
+#  description = "Allow SSH inbound traffic"
+  vpc_id= "vpc-07d2b7ba3dfc13689"
 }
+#      ingress = [
+#        {
+#          description      = "SSH from VPC"
+#          from_port        = 22
+#          to_port          = 22
+#          protocol         = "tcp"
+#          cidr_blocks      = ["0.0.0.0/0"]
+#          ipv6_cidr_blocks = ["::/0"]
+#          prefix_list_ids = null,
+#          security_groups = null,
+#          self            = null
+#        },
+#        {
+#          description      = "SSH from VPC"
+#          from_port        = 80
+#          to_port          = 80
+#          protocol         = "tcp"
+#          cidr_blocks      = ["0.0.0.0/0"]
+#          ipv6_cidr_blocks = ["::/0"]
+#          prefix_list_ids = null,
+#          security_groups = null,
+#          self            = null
+#        }
+#      ]
+#
+#      egress = [
+#        {
+#          from_port        = 0
+#          to_port          = 0
+#          protocol         = "-1"
+#          cidr_blocks      = ["0.0.0.0/0"]
+#          ipv6_cidr_blocks = ["::/0"],
+#          prefix_list_ids  = null,
+#          security_groups  = null,
+#          self             = null,
+#          description      = "Libera dados da rede interna"
+#        }
+#      ]
+
+#      tags = {
+#        Name = "allow_ssh"
+#      }
+#    }
